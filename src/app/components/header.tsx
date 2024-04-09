b98eb924cd1014bda5e7a29039b1b00dd5c6a741
Joseph Kelley <josephkelley91@gmail.com>
2024-04-08 21:30:03 -0400

    Clean up border color on header icon.

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -5,7 +5,7 @@ import Breadcrumbs from "./common/breadcrumbs";
 export default function Header() {
     return (
         <main className="header flex justify-start items-center space-x-1 border-b-2">
-            <div className="mt-3 mr-2 mb-4 ml-4 p-1 border-2 border-gray-400 rounded-md">
+            <div className="mt-3 mr-2 mb-4 ml-4 p-1 border-2 border-gray-300 rounded-md">
                 <img
                     src="/query-stats.svg"
                     alt="Tasks Dashboard"
