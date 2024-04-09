d8028000377633ef9bbc04cb6f445a021f2916e2
Joseph Kelley <josephkelley91@gmail.com>
2024-04-08 20:55:12 -0400

    Styled the controls component, added new icon for use in add chart button.

--- src/app/components/common/controls.tsx
+++ src/app/components/common/controls.tsx
@@ -1,7 +1,15 @@
 export default function Controls() {
     return (
-        <div>
-            <p>Controls</p>
+        <div className="controls mt-3 pb-4 border-b-2">
+            <button className="flex ml-4 bg-sky-500 hover:bg-blue-500 text-white py-2 pr-3 pl-2 rounded-md">
+                <img
+                    src="/add.svg"
+                    alt="Add Chart"
+                    width={23}
+                    height={23}
+                />
+                <span className="pl-2">Add chart</span>
+            </button>
         </div>
     )
 }
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,7 +4,7 @@ import Breadcrumbs from "./common/breadcrumbs";
 
 export default function Header() {
     return (
-        <main className="flex justify-start items-center space-x-1 border-b-2">
+        <main className="header flex justify-start items-center space-x-1 border-b-2">
             <div className="mt-3 mr-2 mb-4 ml-4 p-1 border-2 border-gray-400 rounded-md">
                 <img
                     src="/query-stats.svg"
