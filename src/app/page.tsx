eda7ad755e332eeb138f3e3fa814c44a9996317f
Joseph Kelley <josephkelley91@gmail.com>
2024-04-08 20:34:12 -0400

    Moved the controls component to a new common folder to encourage a more logical folder structure.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Header from './components/header';
-import Controls from './components/controls';
+import Controls from './components/common/controls';
 import Dashboard from './components/dashboard';
 
 export default function Home() {
