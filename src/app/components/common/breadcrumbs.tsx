86bb5f46a70783ef09fa5e858d5445749b9f972e
Joseph Kelley <josephkelley91@gmail.com>
2024-04-08 20:32:39 -0400

    Moved the breadcrumbs into it's own component and styled header.

---
+++ src/app/components/common/breadcrumbs.tsx
@@ -0,0 +1,8 @@
+export default function Breadcrumbs() {
+    return (
+        <div className="breadcrumbs">
+            <p className="text-sm font-semibold text-slate-500">Reporting</p>
+            <p className="text-lg font-semibold">Tasks Dashboard</p>
+        </div>
+    )
+}
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
+import Breadcrumbs from "./common/breadcrumbs";
+
 
 export default function Header() {
     return (
-        <div>
-            <p>Header</p>
-        </div>
+        <main className="flex justify-start items-center space-x-1 border-b-2">
+            <div className="mt-3 mr-2 mb-4 ml-4 p-1 border-2 border-gray-400 rounded-md">
+                <img
+                    src="/query-stats.svg"
+                    alt="Tasks Dashboard"
+                    width={30}
+                    height={30}
+                />
+            </div>
+            <Breadcrumbs />
+        </main>
     )
 }
