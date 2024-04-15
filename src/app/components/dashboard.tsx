9c710cd753dd8368c6e10f462d3db4b2a3a0ebe4
Joseph Kelley <josephkelley91@gmail.com>
2024-04-14 20:59:42 -0400

    Invokes call to get tasks and passes returned tasks data to the report container component.

--- src/app/components/dashboard.tsx
+++ src/app/components/dashboard.tsx
@@ -1,12 +1,15 @@
+'use client';
+
 import ReportContainer from "./common/report-container";
+import { GetTasks } from "../api/route";
+import { use } from "react";
 
 export default function Dashboard() {
+    const taskData = use(GetTasks());
+
     return (
         <div>
-            <ReportContainer />
-            <ReportContainer />
-            <ReportContainer />
-            <ReportContainer />
+            <ReportContainer taskData={{ taskData }} />
         </div>
     )
 }
