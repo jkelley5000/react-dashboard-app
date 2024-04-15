b070f71695e38a5aeefcd2641e1340f3bf1a291c
Joseph Kelley <josephkelley91@gmail.com>
2024-04-14 21:00:44 -0400

    Utilizes chart js library to configure a bar chart to render the processed tasks status data.

--- src/app/components/common/report-container.tsx
+++ src/app/components/common/report-container.tsx
@@ -1,5 +1,67 @@
-export default function ReportContainer() {
+import { TaskModel } from "@/app/models/task-model";
+import {
+    Chart as ChartJS,
+    BarElement,
+    Tooltip,
+    Legend,
+    Title,
+    LinearScale,
+    CategoryScale
+} from "chart.js";
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+    BarElement,
+    Tooltip,
+    Legend,
+    Title,
+    LinearScale,
+    CategoryScale
+);
+
+export default function ReportContainer({ taskData }) {
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top' as const,
+            },
+            title: {
+                display: true,
+                text: 'Tasks by Status',
+            },
+        },
+    };
+    const labels = ['On Track', 'At Risk', 'Off Track', 'Complete'];
+    let datasetDataCounts = [0, 0, 0, 0];
+    taskData.taskData.forEach((task: TaskModel) => {
+        if (task.status === 'on track') {
+            datasetDataCounts[0]++;
+        }
+        if (task.status === 'at risk') {
+            datasetDataCounts[1]++;
+        }
+        if (task.status === 'off track') {
+            datasetDataCounts[2]++;
+        }
+        if (task.status === 'complete') {
+            datasetDataCounts[3]++;
+        }
+    });
+    let data = {
+        labels,
+        datasets: [
+            {
+                label: 'Tasks',
+                data: datasetDataCounts,
+                backgroundColor: '#e22ba3',
+            },
+        ],
+    };
+
     return (
-        <main className="report-container p-3 border-2 border-gray-300 rounded-md"></main>
+        <main className="report-container p-3 border-2 border-gray-300 rounded-md">
+            <Bar options={options} data={data} />
+        </main>
     )
 }
