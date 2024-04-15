1b57405bcb43fae5f995fa2530da9206e5b6c4ed
Joseph Kelley <josephkelley91@gmail.com>
2024-04-14 21:01:37 -0400

    Clean up.

--- src/app/components/common/breadcrumbs.tsx
+++ src/app/components/common/breadcrumbs.tsx
@@ -2,7 +2,7 @@ export default function Breadcrumbs() {
     return (
         <div className="breadcrumbs">
             <p className="text-sm font-semibold text-slate-500">Reporting</p>
-            <p className="text-lg font-semibold">Tasks Dashboard</p>
+            <p className="text-lg font-semibold text-black">Tasks Dashboard</p>
         </div>
     )
 }
