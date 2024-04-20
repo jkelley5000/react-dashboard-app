50131daea79dba9ce04ac1459094beab5bab4a88
Joseph Kelley <josephkelley91@gmail.com>
2024-04-20 19:12:23 -0400

    Utilizes vercels image property as opposed to the html img element for enhanced performance.

--- src/app/components/common/controls.tsx
+++ src/app/components/common/controls.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image"
+
 export default function Controls() {
     return (
         <div className="controls mt-3 pb-4 border-b-2">
             <button className="flex ml-4 bg-sky-500 hover:bg-blue-500 text-white py-2 pr-3 pl-2 rounded-md">
-                <img
+                <Image
                     src="/add.svg"
                     alt="Add Chart"
                     width={23}
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     return (
         <main className="header flex justify-start items-center space-x-1 border-b-2">
             <div className="mt-3 mr-2 mb-4 ml-4 p-1 border-2 border-gray-300 rounded-md">
-                <img
+                <Image
                     src="/query-stats.svg"
                     alt="Tasks Dashboard"
                     width={30}
